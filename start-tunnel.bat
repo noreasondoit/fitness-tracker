155eecf890cda3c397a773abe814cf24920e48e4
noreasondoit <noreasondoit@users.noreply.github.com>
2026-05-18 20:41:32 +0300

    Add .gitignore, cleanup junk files (logs, caches, tunnel scripts)

--- start-tunnel.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d C:\Users\otter\bore
-start /min bore.exe local 5000 --to bore.pub > C:\Users\otter\dev\fitness-tracker\bore.log 2>&1
--- tunnel.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=NUL -o ServerAliveInterval=30 -o ServerAliveCountMax=3 -R 80:localhost:5000 nokey@localhost.run 2> "%USERPROFILE%\dev\fitness-tracker\tunnel-output.txt"
